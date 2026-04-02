273636c45d80ffc9699df7d8226510c15bd1617b
nadhifdiaz01 <nadhifdiaz01@gmail.com>
2026-04-02 06:35:56 +0000

    update

--- Halaman2/script.js
+++ Halaman2/script.js
@@ -4,6 +4,7 @@ let isiTabel = document.getElementById('isi');
 tombolKirim.addEventListener('click', function(event) {
     event.preventDefault(); 
 
+    let Nama = document.getElementById('NamaInput').value;
     let noRumah = document.getElementById('noRumahInput').value;
     let pesan = document.getElementById('pesanInput').value;
     let tanggalInput = document.getElementById('tanggalInput').value;
@@ -24,14 +25,28 @@ tombolKirim.addEventListener('click', function(event) {
         return;
     }
 
+    if (noRumah < 20 && noRumah > 0) {
+        rute = "A";
+    } else if (noRumah < 40 && noRumah >= 20) {
+        rute = "B";
+    } else if (noRumah <= 60 && noRumah >= 40) {
+        rute = "C";
+    } else {
+        alert('Nomor rumah tidak valid!');
+        return;
+    }
+
     let barisBaru = document.createElement('tr');
 
     barisBaru.innerHTML = `
+        <td>${Nama}</td>
+        <td>${rute}</td>
         <td>${noRumah}</td>
         <td>${pesan}</td>
         <td>${waktu}</td>
         <td>
             <button class="btn-hapus">Hapus</button>
+            <button class="btn-edit">Edit</button>
         </td>
     `;
 
@@ -46,6 +61,8 @@ tombolKirim.addEventListener('click', function(event) {
 
     alert('Pesan anda sudah terkirim');
     
+    document.getElementsByName('NamaInput').value = "";
+    document.getElementById('ruteInput').value = "";
     document.getElementById('noRumahInput').value = "";
     document.getElementById('pesanInput').value = "";
 });
